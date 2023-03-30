51304c6a173339b14bbc9129053a76cccd56918c
Hilmi Almuhtade Billah <hilmialmuhtadeb@gmail.com>
2023-03-31 01:57:37 +0700

    fix sw issues

--- public/firebase-messaing-sw.js
+++ public/firebase-messaing-sw.js
@@ -5,21 +5,30 @@ firebase.initializeApp({
     'messagingSenderId': '235318415040'
 });
 
-const messaging = firebase.messaging();
+firebase.messaging();
 
-firebase.onBackgroundMessage(messaging, (payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message', payload);
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('../firebase-messaging-sw.js')
+//         .then(function(registration) {
+//         console.log('Registration successful, scope is:', registration.scope);
+//         }).catch(function(err) {
+//         console.log('Service worker registration failed, error:', err);
+//         });
+// }
+
+// firebase.onBackgroundMessage(messaging, (payload) => {
+//     console.log('[firebase-messaging-sw.js] Received background message', payload);
     
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png'
-    };
+//     const notificationTitle = 'Background Message Title';
+//     const notificationOptions = {
+//         body: 'Background Message body.',
+//         icon: '/firebase-logo.png'
+//     };
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
-});
+//     self.registration.showNotification(notificationTitle, notificationOptions);
+// });
 
-firebase.onMessage(messaging, (payload) => {
-    console.log('Message received. ', payload);
-    // ...
-  });
+// firebase.onMessage(messaging, (payload) => {
+//     console.log('Message received. ', payload);
+//     // ...
+//   });
--- src/features/messaging/push-notification.js
+++ src/features/messaging/push-notification.js
@@ -4,6 +4,7 @@ import { getMessaging, getToken } from "firebase/messaging";
 function requestPermission() {
   Notification.requestPermission().then(async (permission) => {
     if (permission === 'granted') {
+      console.log('masuk granted');
       const messaging = getMessaging()
       const token = await getToken(messaging, {
         vapidKey: 'BDcy6eVBfpMiNQ0FKHi4vpUCHHrPo7R6aEp_EEFw_aMiOJuDjVZst5B5pnE-poqsPhe4BnmBeK1ivqXfsw93DUI'
